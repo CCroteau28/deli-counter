2d72a3b6859538425f378a7a0537a15195ae38af
CCroteau28 <ccroteau28@gmail.com>
2021-03-02 11:50:55 -0700

    commit tree 5cd9b242c1ea6bb299ef3d68bfb6f736bf11affe

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+require "pry" # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+    position = "The line is currently:"
+    if katz_deli.length == 0
+    puts "The line is currently empty."
+    else 
+        katz_deli.each_with_index {|val, index| position << " #{index + 1}. #{val}"}
+        puts position 
+end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+    if katz_deli.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{katz_deli.shift}."
+    end
+end   
